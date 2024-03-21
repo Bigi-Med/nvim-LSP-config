0e55bedd7360fb68026b6ed991a36afce379c810
simo <simo@LAPTOP-UENQOUFM.localdomain>
2024-03-21 21:03:25 +0100

    MBI: cursor config

--- lua/simo/set.lua
+++ lua/simo/set.lua
@@ -1,6 +1,7 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.guicursor = ""
+vim.opt.guicursor = 'n-v-c:block-Cursor'
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -12,3 +13,7 @@ vim.opt.smartindent = true
 vim.opt.scrolloff = 8
 
 vim.opt.updatetime = 50
+
+function setCursor()
+    vim.api.nvim_set_hl(0,"CrusorReset",{fg='white',bg='white'})
+end
