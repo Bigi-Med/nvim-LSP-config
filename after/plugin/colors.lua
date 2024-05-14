6f2c8b881c9cff4e52240438468853b54b1315fd
simo <simo@LAPTOP-UENQOUFM.localdomain>
2024-05-14 23:18:33 +0200

    MBI: change background Color

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -2,8 +2,8 @@ function ColorMyPencils(color)
 	color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0,"Normal", {bg = "none" })
-	vim.api.nvim_set_hl(0,"Normalfloat", {bg = "none" })
+	vim.api.nvim_set_hl(0,"Normal", {bg = "#010224" })
+	vim.api.nvim_set_hl(0,"Normalfloat", {bg = "#0000FF" })
 
 end
 
