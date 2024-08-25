45e464bde5e9e3f55be9662bce459025c2dd3161
Bigi-Med <medbigi2000@gmail.com>
2024-08-25 13:40:05 +0200

    MBI: resolved

--- lua/simo/set.lua
+++ lua/simo/set.lua
@@ -29,3 +29,14 @@ vim.opt.updatetime = 50
 function setCursor()
     vim.api.nvim_set_hl(0,"CrusorReset",{fg='white',bg='white'})
 end
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.updatetime = 50
